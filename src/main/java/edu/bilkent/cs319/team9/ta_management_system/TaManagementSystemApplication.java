71ea6bad06e0acb7fc61f289800f268dc9f2fc1b
Deniz <d.ozturk@ug.bilkent.edu.tr>
2025-04-06 15:04:11 +0300

    Initial backend setup

---
+++ src/main/java/edu/bilkent/cs319/team9/ta_management_system/TaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.bilkent.cs319.team9.ta_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/bilkent/cs319/team9/ta_management_system/TaManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bilkent.cs319.team9.ta_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
